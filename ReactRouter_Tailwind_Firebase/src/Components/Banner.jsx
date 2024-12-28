56a743a8bb6651db2eb19aabbc2a51eddbefc9ba
Noor-A-Alam <noorsomrat2000@gmail.com>
2024-12-29 00:48:28 +0600

    Home Page Swiper

---
+++ ReactRouter_Tailwind_Firebase/src/Components/Banner.jsx
@@ -0,0 +1,26 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+const Banner = () => {
+    return (
+        <div >
+            <Carousel >
+                <div >
+                    <img src="../../public/food_web_banner_29.jpg" />
+
+                </div>
+                <div className="">
+                    <img src="../../public/food_web_banner_29.jpg" />
+
+                </div>
+                <div>
+                    <img src="../../public/food_web_banner_29.jpg" />
+
+                </div>
+
+
+            </Carousel>
+        </div >
+    );
+};
+
+export default Banner;
---
+++ ReactRouter_Tailwind_Firebase/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "./Banner";
+import Swipper from "./Swipper";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Swipper></Swipper>
+
+        </div>
+    );
+};
+
+export default Home;
